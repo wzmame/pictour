80818c4ef62becb1ddf2fe3fae1ee33a14570d57
Jeffrey Martinez <jemartinez@groupon.com>
2015-11-20 09:53:06 -0800

    Add parse config file

--- app/src/main/java/wszolek/lauren/pictour/ParseConfig.java
+++ app/src/main/java/wszolek/lauren/pictour/ParseConfig.java
@@ -1,6 +1,9 @@
 package wszolek.lauren.pictour;
 
 public final class ParseConfig {
-    public static String APPLICATION_ID = "[insert app id here]";
-    public static String CLIENT_ID = "[insert client id here]";
+    public static String CONFIG_FILENAME = "parse-configuration.json";
+    
+    // These will eventually be pulled from a config file
+    public static String APPLICATION_ID = "[insert app id]";
+    public static String CLIENT_ID = "[insert client id]";
 }
