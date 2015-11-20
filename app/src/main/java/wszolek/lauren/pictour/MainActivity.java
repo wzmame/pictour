52abd3282ca8203db58cb6e03cf1716a914df74b
Jeffrey Martinez <jemartinez@groupon.com>
2015-11-20 09:43:20 -0800

    Parse connectivity test

--- app/src/main/java/wszolek/lauren/pictour/MainActivity.java
+++ app/src/main/java/wszolek/lauren/pictour/MainActivity.java
@@ -1,7 +1,10 @@
 package wszolek.lauren.pictour;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, ParseConfig.APPLICATION_ID, ParseConfig.CLIENT_ID);
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
     }
 }
---
+++ app/src/main/java/wszolek/lauren/pictour/ParseConfig.java
@@ -0,0 +1,6 @@
+package wszolek.lauren.pictour;
+
+public final class ParseConfig {
+    public static String APPLICATION_ID = "[insert app id here]";
+    public static String CLIENT_ID = "[insert client id here]";
+}
