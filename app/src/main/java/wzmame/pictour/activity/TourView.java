38362b12ef452a5e7c821fa04765f35f7b18d129
jeff martinez <jeffbmartinez@gmail.com>
2015-11-25 23:29:51 -0800

    Remove unused menu code to fix compilation issues

--- app/src/main/java/wzmame/pictour/activity/TourView.java
+++ app/src/main/java/wzmame/pictour/activity/TourView.java
@@ -1,13 +1,11 @@
 package wzmame.pictour.activity;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.astuetz.PagerSlidingTabStrip;
 
@@ -27,28 +25,6 @@ public class TourView extends AppCompatActivity {
         vPager.setAdapter(adapter);
         PagerSlidingTabStrip tabStrip = (PagerSlidingTabStrip) findViewById(R.id.tabs);
         tabStrip.setViewPager(vPager);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_tour_view, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     private class TourViewPagerAdapter extends FragmentPagerAdapter {
