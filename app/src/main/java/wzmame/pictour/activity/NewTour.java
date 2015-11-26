d93cd62195f95b88485471d0ededac6ae5353c48
jeff martinez <jeffbmartinez@gmail.com>
2015-11-25 21:16:37 -0800

    Make tour name a required field when activating Done button

--- app/src/main/java/wzmame/pictour/activity/NewTour.java
+++ app/src/main/java/wzmame/pictour/activity/NewTour.java
@@ -3,6 +3,7 @@ package wzmame.pictour.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import wzmame.pictour.R;
@@ -17,6 +18,13 @@ public class NewTour extends AppCompatActivity {
 
     public void onDoneAction(View view) {
         Toast.makeText(this, "Done button!", Toast.LENGTH_SHORT).show();
+
+        EditText etName = (EditText) findViewById(R.id.etName);
+        String name = etName.getText().toString();
+        if (name.isEmpty()) {
+            Toast.makeText(this, "Tour name is required", Toast.LENGTH_LONG).show();
+            return;
+        }
     }
 
     public void onAddLocationAction(View view) {
