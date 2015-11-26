b85feeef5f09940e0fc13b24616c42fbbf9c38b5
jeff martinez <jeffbmartinez@gmail.com>
2015-11-25 23:02:30 -0800

    connect 'done' button in tour-creation activity

--- app/src/main/java/wzmame/pictour/activity/NewTour.java
+++ app/src/main/java/wzmame/pictour/activity/NewTour.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -16,28 +15,23 @@ public class NewTour extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_tour);
-        Button btnAddLocation = (Button) findViewById(R.id.btnAddLocation);
-        btnAddLocation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(NewTour.this, NewLocation.class);
-                startActivity(i);
-            }
-        });
     }
 
     public void onDoneAction(View view) {
-        Toast.makeText(this, "Done button!", Toast.LENGTH_SHORT).show();
-
         EditText etName = (EditText) findViewById(R.id.etName);
         String name = etName.getText().toString();
         if (name.isEmpty()) {
             Toast.makeText(this, "Tour name is required", Toast.LENGTH_LONG).show();
             return;
         }
+
+        // TODO (https://github.com/wzmame/pictour/issues/20) Store the new tour in the backend.
+
+        finish();
     }
 
     public void onAddLocationAction(View view) {
-        Toast.makeText(this, "Add Location button!", Toast.LENGTH_SHORT).show();
+        Intent addLocationIntent = new Intent(this, NewLocation.class);
+        startActivity(addLocationIntent);
     }
 }
