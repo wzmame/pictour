c31c395c1fa85eeffd1c28c2385db5f5b110332e
Francis Meng <xmeng@groupon.com>
2015-12-08 00:28:57 -0800

    remove client id

--- app/src/main/java/wzmame/pictour/config/ParseConfig.java
+++ app/src/main/java/wzmame/pictour/config/ParseConfig.java
@@ -5,7 +5,6 @@ public final class ParseConfig {
     public static String CONFIG_FILENAME = "parse-configuration.json";
     
     // These will eventually be pulled from a config file
-    public static String APPLICATION_ID = "X5LsrAic8YtqlTToIxmTXCqjh9ppPDlTKy8qX7iH";
-    public static String CLIENT_ID = "fe88nGYlpfamdfoUPVNq6ngX9FvSc9ydMrxEZOgx";
-    public static String GOOGLE_MAP_API_KEY = "AIzaSyCgCPKirhI-cJfcS5IPbq8vT33D6p17Vbg";
+    public static String APPLICATION_ID = "[insert app id]";
+    public static String CLIENT_ID = "[insert client id]";
 }
