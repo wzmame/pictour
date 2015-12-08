464e17a2a788d6602635c84f0b32172ba9240b3a
Francis Meng <xmeng@groupon.com>
2015-12-08 00:28:15 -0800

    change miner style remove client id

--- app/src/main/java/wzmame/pictour/config/ParseConfig.java
+++ app/src/main/java/wzmame/pictour/config/ParseConfig.java
@@ -5,6 +5,6 @@ public final class ParseConfig {
     public static String CONFIG_FILENAME = "parse-configuration.json";
     
     // These will eventually be pulled from a config file
-    public static String APPLICATION_ID = "[App-ID]";
-    public static String CLIENT_ID = "[Client-ID]";
+    public static String APPLICATION_ID = "[insert app id]";
+    public static String CLIENT_ID = "[insert client id]";
 }
